29634f2eaad31af967598ba4f334d7403c130e09
Lisabessa <lisabessa.neyz@gmail.com>
2025-01-27 03:28:18 +0700

    Change a bit

--- server.py
+++ server.py
@@ -4,19 +4,19 @@ HOST = 'localhost'
 PORT = 9095
 
 async def handle_echo(reader, writer):
-    client_address = writer.get_extra_info('peername')
-    print(f'Подключен новый клиент: {client_address}')
+    host, port = writer.get_extra_info('peername')
+    print(f'Подключен новый клиент: [{host}:{port}]')
 
     while True:
         # Чтение данных от клиента
         data = await reader.read(100)
 
         if not data:
-            print("Отключение клиента")
+            print(f'Отключение клиента [{host}:{port}]')
             break
 
         message = data.decode()
-        print(message)
+        print(f'[{host}:{port}]: {message}')
 
         # Отправка данных обратно клиенту
         writer.write(data)
@@ -31,7 +31,8 @@ async def main():
     server = await asyncio.start_server(handle_echo, HOST, PORT)
 
     # Вывод информации о сервере
-    print(f'Сервер на {server.sockets[0].getsockname()}')
+    host, port = server.sockets[0].getsockname()
+    print(f'Сервер на [{host}:{port}]')
 
     async with server:
         await server.serve_forever()
